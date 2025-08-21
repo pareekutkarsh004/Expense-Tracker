98813177cbc389305ea07d3a7e59ddd9ddcd9891
Utkarsh <utkarshpareek04@gmail.com>
2025-08-21 16:03:42 +0530

    Setup for backend and frontend done

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,1 +1,13 @@
-import express from express
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/',(req,res)=>{
+    res.send('HELLO FROM THE BACKEND!')
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
